2702ab6873d43e5a3b2db3b7b3b08f7411a80ff3
emilianojduarte <emilianojduarte@gmail.com>
2024-08-17 18:42:36 -0300

    Update 349

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://oaeemifugqebuzqibiwc.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9hZWVtaWZ1Z3FlYnV6cWliaXdjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM5MTg0NTYsImV4cCI6MjAzOTQ5NDQ1Nn0.BUAVWc1LNzdrT5lOkSwh5ITbeOcQNxD-NK9nKPet1g8";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
