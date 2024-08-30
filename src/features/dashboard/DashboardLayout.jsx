643a3f090899c03b2f70644034cdffe7e01f103e
Emiliano Duarte <94936967+emilianojduarte@users.noreply.github.com>
2024-08-30 20:23:41 -0300

    Update 400

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import { useRecentStays } from "./useRecentStays";
+import Spinner from "../../ui/Spinner";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,23 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isPending: isLoading1 } = useRecentBookings();
+  const { stays, confirmedStays, isPending: isLoading2 } = useRecentStays();
+
+  if (isLoading1 || isLoading2) return <Spinner />;
+
+  console.log(bookings);
+
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today Activity</div>
+      <div>Chart stay duration</div>
+      <div>Chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
